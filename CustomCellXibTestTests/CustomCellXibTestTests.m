bd5e4615bd11ca63dea3c97cccc62113d46d6adc
iasandcb <iasandcb@gmail.com>
2014-01-17 17:19:25 +0900

    Initial Commit

---
+++ CustomCellXibTestTests/CustomCellXibTestTests.m
@@ -0,0 +1,34 @@
+//
+//  CustomCellXibTestTests.m
+//  CustomCellXibTestTests
+//
+//  Created by T on 2014. 1. 17..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomCellXibTestTests : XCTestCase
+
+@end
+
+@implementation CustomCellXibTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
